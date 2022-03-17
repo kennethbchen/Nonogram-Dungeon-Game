6215b6d70937d8ef9ffdc5235fc2e059dcf43925
kennethbchen <kennethbchen@gmail.com>
2022-03-16 20:38:04 -0400

    Implemented Trap

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -41,6 +41,9 @@ func _handle_collision(direction: Vector2, collider):
 	if collider.is_in_group("enemy"):
 		attack_character(collider)
 		bump_tween(direction)
+	elif collider is Trap:
+		collider.interact_with(self)
+		move_tween(direction)
 	elif collider is Interactable:
 		collider.interact_with(self)
 		bump_tween(direction)
---
+++ Trap.gd
@@ -0,0 +1,9 @@
+extends Interactable
+
+class_name Trap
+
+export var attack = 2
+
+func interact_with(character: Character):
+	character.take_damage(attack)
+	queue_free()
