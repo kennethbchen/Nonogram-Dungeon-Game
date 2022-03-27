33d277d06fb653135c2e3db70d525332b37bc6d7
kennethbchen <kennethbchen@gmail.com>
2022-03-27 18:07:09 -0400

    Created title screen

---
+++ TitleScreenController.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+
+func _on_Start_Button_pressed():
+	get_tree().change_scene("res://Game.tscn")
+	pass # Replace with function body.
+
+
+func _on_Instruction_Button_pressed():
+	pass # Replace with function body.
